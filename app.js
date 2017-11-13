85aa4e58d137f343fe415e4b0f913bb820b6d67a
Thomas Frank <t@clientsmile.se>
2017-11-13 14:46:06 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Requires
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// Create a new reverse proxy
+const proxy = httpProxy();
+
+// Create a new webserver
+http.createServer((req,res) => {
+  // Can we read the incoming url?
+  res.write(req.url);
+}).listen(80);
