58122b6ac914a9fb45542a01a0fbb729bef88589
Thomas Frank <t@clientsmile.se>
2017-11-13 14:50:20 +0100

    on error

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const httpProxy = require('http-proxy');
 // Create a new reverse proxy
 const proxy = httpProxy();
 
+proxy.on('error',()=>{});
+
 // Create a new webserver
 http.createServer((req,res) => {
   // Can we read the incoming url?
