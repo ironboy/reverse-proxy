f4b8738077bdbe351f7043b58480c1c71c2a9e4c
Thomas Frank <t@clientsmile.se>
2017-11-16 14:39:16 +0100

    renewing certs automatically

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const httpProxy = require('http-proxy');
 const tls = require('tls');
 const fs = require('fs');
 const path = require('path');
+const exec = require('child_process').exec;
 
 // Read all certs from certbot into an object
 let certs = readCerts("/etc/letsencrypt/live");
@@ -119,3 +120,18 @@ function readCerts(pathToCerts){
   return certs;
 
 }
+
+function renewCerts(){
+
+  exec('certbot renew',(error,stdOut,stdError)=>{
+    console.log('renewing certs',stdOut);
+    certs = readCerts('/etc/letsencrypt/live');
+  });
+
+}
+
+// Renew certs if needed on start
+renewCerts();
+// and then once every day
+setInterval(renewCerts, 1000 * 60 * 60 * 24);
+
